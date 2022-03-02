2c9195b322fec5ec8f14e018fab4f4cf32bb6406
Hal <hgrforrest@gmail.com>
2022-03-02 16:29:18 -0500

    accessing api for data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+// api key for openweather 6a229679b9f32bbf8e2efa2697417d08
+
+// import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+
+const apiKey = '6a229679b9f32bbf8e2efa2697417d08'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [longitude, setLongitude] = useState(0)
+	const [latitude, setLatitude] = useState(0)
+	const [result, setResult] = useState('')
+
+	useEffect(() => {
+
+		const geolocationCoordinatesInstance = navigator.geolocation.getCurrentPosition((position) => {
+			setLatitude(position.coords.latitude)
+			setLongitude(position.coords.longitude)
+		})
+		if (latitude != 0 && longitude != 0) {
+			axios({
+				// url: `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}`,
+				url: `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`,
+				method: 'GET',
+				dataResponse: 'json'
+			}).then((res) => {
+				setResult(res.data)
+				console.log(res.data)
+			})
+		}
+	}, [longitude, latitude])
+
+	return (
+		<div className="App">
+			{/* weather display from api app */}
+			{result !== '' ?
+				<div className="weatherDisplay">
+					<p>
+						lat: {latitude}
+					</p>
+					<p>
+
+						lon: {longitude}
+					</p>
+					<div className="currentWeather">
+
+						<p>max: {result.daily[0].temp.max}</p>
+						<p>min: {result.daily[0].temp.min}</p>
+					</div>
+					<div>
+						result: {JSON.stringify(result)}
+					</div>
+				</div>
+				:
+				null
+			}
+		</div>
+	);
 }
 
 export default App;
