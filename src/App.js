0d3348fbc74842927952a1012564a3809960cd9e
Hal <hgrforrest@gmail.com>
2022-03-03 16:24:07 -0500

    testing again

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 // import datetime from 'datetime'
 
-const apiKey = process.env.REACT_APP_API_KEY
+const apiKey = '6a229679b9f32bbf8e2efa2697417d08'
 
 const currentDate = new Date()
 
@@ -22,7 +22,7 @@ function App() {
 		// only do an api call if we received geolocation
 		if (latitude !== 0 && longitude !== 0) {
 			axios({
-				url: `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid='6a229679b9f32bbf8e2efa2697417d08'&units=metric`,
+				url: `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`,
 				method: 'GET',
 				dataResponse: 'json'
 			}).then((res) => {
