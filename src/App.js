31fdc331283d6b6d67fc2e77c9bf46b12b996a48
Hal <hgrforrest@gmail.com>
2022-04-14 17:35:44 -0400

    added chart of temps over the next 48 hours

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import DayForecast from './DayForecast';
 import HourForecast from './HourForecast';
+import Chart from 'chart.js/auto'
+import { Bar } from 'react-chartjs-2';
 
 const currentDate = new Date()
 
@@ -11,6 +13,8 @@ function App() {
 	const [longitude, setLongitude] = useState(200) // 200 is outside possible value
 	const [latitude, setLatitude] = useState(200)
 	const [result, setResult] = useState('')
+	const [chartData, setChartData] = useState([])
+	const [labels, setLabels] = useState([])
 
 	useEffect(() => {
 		// request geolocation permission from user
@@ -27,6 +31,15 @@ function App() {
 			}).then((res) => {
 				console.log(res.data)
 				setResult(res.data)
+				const tempData = res.data.hourly.map(element => {
+					return element.temp
+				})
+				setChartData(tempData)
+				const labelData = res.data.hourly.map((element, index) => {
+					return (currentDate.getHours() % 24 > 12 ? (currentDate.getHours() + index) % 12 : currentDate.getHours()) 
+					+ (((currentDate.getHours() + index) % 24) > 11 ? "pm" : "am")
+				})
+				setLabels(labelData)
 			})
 		}
 	}, [longitude, latitude])
@@ -35,40 +48,65 @@ function App() {
 		<div className='App'>
 			{/* weather display from api app */}
 			{result !== '' ?
-			<div class="container">
-				<div className="weatherDisplay">
-					{/* create forecast for first 5 days */}
-					{result.hourly.map((element, index) => {
-						if (index < 5) {
-							const elementDate = new Date()
-							elementDate.setDate(currentDate.getDate() + index)
-							return (
-								<div className={`dayContainer${index} dayContainer`} key={index}>
-									<HourForecast element={element} index={index} elementDate={elementDate} />
-								</div>
-							)
-						} else {
-							return null
-						}
-					})}
+				<div className="container">
+					<Bar
+						data={{
+							labels: labels,
+							datasets: [
+								{
+									label: 'Temperature',
+									backgroundColor: 'rgba(75,192,192,1)',
+									borderColor: 'rgba(0,0,0,1)',
+									borderWidth: 2,
+									data: chartData
+								}
+							]
+						}}
+						options={{
+							title: {
+								display: true,
+								text: 'temperature over the course of the 2 days',
+								fontSize: 20
+							},
+							legend: {
+								display: true,
+								position: 'right'
+							}
+						}}
+					/>
+					<div className="weatherDisplay">
+						{/* create forecast for first 5 days */}
+						{result.hourly.map((element, index) => {
+							if (index < 5) {
+								const elementDate = new Date()
+								elementDate.setDate(currentDate.getDate() + index)
+								return (
+									<div className={`dayContainer${index} dayContainer`} key={index}>
+										<HourForecast element={element} index={index} elementDate={elementDate} />
+									</div>
+								)
+							} else {
+								return null
+							}
+						})}
+					</div>
+					<div className="weatherDisplay">
+						{/* create forecast for first 5 days */}
+						{result.daily.map((element, index) => {
+							if (index < 5) {
+								const elementDate = new Date()
+								elementDate.setDate(currentDate.getDate() + index)
+								return (
+									<div className={`dayContainer${index} dayContainer`} key={index}>
+										<DayForecast element={element} index={index} elementDate={elementDate} />
+									</div>
+								)
+							} else {
+								return null
+							}
+						})}
+					</div>
 				</div>
-				<div className="weatherDisplay">
-					{/* create forecast for first 5 days */}
-					{result.daily.map((element, index) => {
-						if (index < 5) {
-							const elementDate = new Date()
-							elementDate.setDate(currentDate.getDate() + index)
-							return (
-								<div className={`dayContainer${index} dayContainer`} key={index}>
-									<DayForecast element={element} index={index} elementDate={elementDate} />
-								</div>
-							)
-						} else {
-							return null
-						}
-					})}
-				</div>
-			</div>
 				:
 				'Geolocation not supported'
 			}
