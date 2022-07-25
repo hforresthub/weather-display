0709871a3dd83b9dca18555b501333601dadb2ef
Hal <hgrforrest@gmail.com>
2022-07-25 13:07:38 -0400

    navigation style changes, other minor style changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import { useRef } from 'react';
 import Weather from './components/Weather';
 import News from './components/News';
 
+//fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCloudSunRain, faCommentDots, faNewspaper } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCloudSunRain, faNewspaper, faCommentDots)
+
 function App() {
 	// button variables
 	const myRef = [useRef(null), useRef(null), useRef(null), useRef(null), useRef(null), useRef(null)]
@@ -24,23 +31,23 @@ function App() {
 			<nav>
 				<ul>
 					<li>
-						<button onClick={executeScroll(0)} className='sectionButton'> Weather </button>
+						<button onClick={executeScroll(0)} className='sectionButton'><FontAwesomeIcon icon="fa-solid fa-cloud-sun-rain" /><span> Weather</span></button>
 						<button onClick={handleButtonClick(0)} className='minimizerButton'>{sectionToggles[0] ? `-` : `+`}</button>
 					</li>
-					<li>
+					{/* <li>
 						<button onClick={executeScroll(1)} className='sectionButton'> Charts </button>
 						<button onClick={handleButtonClick(1)} className='minimizerButton'>{sectionToggles[1] ? `-` : `+`}</button>
-					</li>
+					</li> */}
 					<li>
-						<button onClick={executeScroll(2)} className='sectionButton'> News </button>
+						<button onClick={executeScroll(2)} className='sectionButton'><FontAwesomeIcon icon="fa-solid fa-newspaper" /><span> News</span></button>
 						<button onClick={handleButtonClick(2)} className='minimizerButton'>{sectionToggles[2] ? `-` : `+`}</button>
 					</li>
-					<li>
+					{/* <li>
 						<button onClick={executeScroll(3)} className='sectionButton'> Saved </button>
 						<button onClick={handleButtonClick(3)} className='minimizerButton'>{sectionToggles[3] ? `-` : `+`}</button>
-					</li>
+					</li> */}
 					<li>
-						<button onClick={executeScroll(4)} className='sectionButton'> Threads </button>
+						<button onClick={executeScroll(4)} className='sectionButton'><FontAwesomeIcon icon="fa-solid fa-comment-dots" /><span> Threads</span></button>
 						<button onClick={handleButtonClick(4)} className='minimizerButton'>{sectionToggles[4] ? `-` : `+`}</button>
 					</li>
 				</ul>
--- src/components/News.js
+++ src/components/News.js
@@ -45,14 +45,14 @@ const News = ({ handleButtonClick, sectionToggles, myRef }) => {
 	// for news api data
 	useEffect(() => {
 		// NewsAPI
-		axios({
-			url: `https://api.thenewsapi.com/v1/news/all?locale=us,ca&language=en&api_token=${process.env.REACT_APP_NEWS_API_KEY_2}`,
-			method: 'GET',
-			dataResponse: 'json'
-		}).then((res) => {
-			// console.log(JSON.stringify(res.data.articles))
-			setNewsArticles(res.data.data)
-		})
+		// axios({
+		// 	url: `https://api.thenewsapi.com/v1/news/all?locale=us,ca&language=en&api_token=${process.env.REACT_APP_NEWS_API_KEY_2}`,
+		// 	method: 'GET',
+		// 	dataResponse: 'json'
+		// }).then((res) => {
+		// 	// console.log(JSON.stringify(res.data.articles))
+		// 	setNewsArticles(res.data.data)
+		// })
 		// console.log(JSON.stringify(newsArticles))
 	}, [])
 
