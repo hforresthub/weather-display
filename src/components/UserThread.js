d538abb5ba65c1f70c038dc13bf837a9560ef752
Hal <hgrforrest@gmail.com>
2022-10-18 14:49:12 -0400

    new image for user threads

--- src/components/UserThread.js
+++ src/components/UserThread.js
@@ -5,7 +5,7 @@ const UserThread = ({ element, index }) => {
 				<h3 title={element.title}>
 					{element.title ? element.title : 'No title available'}
 				</h3>
-				<img src={element.image_url} onError={(e) => {e.target.src=require(`../images/backupImage.png`)}} alt={`Image related to ${element.title}`} />
+				<img src={element.image_url} onError={(e) => {e.target.src=require(`../images/threadsBackupImage.png`)}} alt={`Image related to ${element.title}`} />
 			</div>
 			<p className='content'>{element.description ? element.description : 'No description available'}</p>
 		</div>
